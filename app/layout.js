688394b66611343eafbccb689ead9523db0ed5ac
Shippu Kumar <Itzshippu@gmail.com>
2024-03-23 16:11:05 +0530

    dsfsdg

--- app/layout.js
+++ app/layout.js
@@ -8,29 +8,29 @@ import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: '91 Club Official - 91 Clubs 100% trusted website play and win money',
-  description: 'Join 91 Club Official to play thrilling games like Aviator, Win Go, sports, and more. Compete and win real money prizes! Join now for the ultimate gaming experience!',
+  title: '91 Club - Play and Win Real Money Games | 91 Clubs',
+  description: 'Join 91 Club to play exciting games like Aviator, Win Go, sports, and more. Compete and win real money prizes! Sign up now for the ultimate gaming experience!',
   generator: '91 Clubs',
   applicationName: '91 Clubs',
   referrer: 'origin-when-cross-origin',
-  keywords: ['91club, 91 club, 91 club vip, 91 club register, 91 club app, 91 club online game'],
-  authors: [{ name: 'Rahul Kumar' }, { name: 'Rahul Kumar', url: 'https://www.91clubs.info/' }],
+  keywords: ['91 club, 91 club games, real money games, online gaming, 91 club app'],
+  authors: [{ name: 'Rahul Kumar' }],
   creator: 'Rahul Kumar',
   publisher: '91 Club APP',
   formatDetection: {
-    email: 'asterioamara@gmail.com',
+    email: 'support@91clubs.info',
     address: false,
     telephone: false,
   },
   metadataBase: new URL('https://www.91clubs.info/'),
   alternates: {
-    canonical: '/',
+    canonical: 'https://www.91clubs.info/',
   },
   openGraph: {
-    title: '91 Club Official - 91 Clubs 100% trusted website play and win money',
-    description: 'Join 91 Club Official to play thrilling games like Aviator, Win Go, sports, and more. Compete and win real money prizes! Join now for the ultimate gaming experience!',
+    title: '91 Club - Play and Win Real Money Games | 91 Clubs',
+    description: 'Join 91 Club to play exciting games like Aviator, Win Go, sports, and more. Compete and win real money prizes! Sign up now for the ultimate gaming experience!',
     url: 'https://www.91clubs.info/',
-    siteName: '91 club',
+    siteName: '91 Club',
     images: [
       {
         url: 'https://www.91clubs.info/og-img.jpg',
@@ -56,8 +56,8 @@ export const metadata = {
   },
   twitter: {
     card: 'summary_large_image',
-    title: '91 Club Official - 91 Clubs 100% trusted website play and win money',
-    description: 'Join 91 Club Official to play thrilling games like Aviator, Win Go, sports, and more. Compete and win real money prizes! Join now for the ultimate gaming experience!',
+    title: '91 Club - Play and Win Real Money Games | 91 Clubs',
+    description: 'Join 91 Club to play exciting games like Aviator, Win Go, sports, and more. Compete and win real money prizes! Sign up now for the ultimate gaming experience!',
     siteId: '1467726470533754880',
     creator: '@91club',
     creatorId: '1467726470533754880',
