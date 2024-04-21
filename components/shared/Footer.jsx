8f13e3e9ee2af963e4b574854e47affe9a2e7c53
Shippu Kumar <Itzshippu@gmail.com>
2024-04-21 16:11:30 +0530

    vcre

--- components/shared/Footer.jsx
+++ components/shared/Footer.jsx
@@ -10,6 +10,7 @@ const Footer = () => {
       </div>
       <div className='text-lg' id='contact-us'>
         <p className='text-white'><strong>Email: </strong><a href="mailto:asterioamara@gmail.com">asterioamara@gmail.com</a></p>
+        <p className='text-white'><strong>Telegram: </strong><a href="https://t.me/amara_1010">asterioamara</a></p>
         <div>
           <p className='text-white'><strong>Webstie</strong>: <a href="https://wwww.91service.in/wap/indexE.jsp#" className='font-semibold underline underline-offset-4 '>91Club Service Portal</a></p>
         </div>
